b2f6ed8df6079fd6816c585518e819b054b6479b
Kleinmergi <mathias.mergenthaler@me.com>
2026-03-24 10:38:26 +0100

    Add example and README docs for extended UI controls

---
+++ src/button.cpp
@@ -0,0 +1,69 @@
+#include "phyphoxBleExperiment.h"
+#include "copyToMem.h"
+
+void PhyphoxBleExperiment::Button::setOutputChannel(int b)
+{
+    ERROR = ERROR.MESSAGE == NULL ? err_checkUpper(b, 5, "setOutputChannel") : ERROR;
+    char tmp[20];
+    sprintf(tmp, "CB%i", b);
+    copyToMem(&OUTPUT, tmp);
+}
+
+void PhyphoxBleExperiment::Button::setChannel(int b)
+{
+    setOutputChannel(b);
+}
+
+void PhyphoxBleExperiment::Button::setValue(float value)
+{
+    char tmp[20];
+    snprintf(tmp, sizeof(tmp), "%.6g", value);
+    copyToMem(&VALUE, tmp);
+}
+
+void PhyphoxBleExperiment::Button::setXMLAttribute(const char *xml)
+{
+    ERROR = ERROR.MESSAGE == NULL ? err_checkLength(xml, 98, "setXMLAttribute") : ERROR;
+    copyToMem(&XMLAttribute, (" " + std::string(xml)).c_str());
+}
+
+void PhyphoxBleExperiment::Button::getBytes(char *buffArray)
+{
+    strcat(buffArray, "\t\t<button");
+    if (LABEL)
+    {
+        strcat(buffArray, " label=\"");
+        strcat(buffArray, LABEL);
+        strcat(buffArray, "\"");
+    }
+    else
+    {
+        strcat(buffArray, " label=\"button\"");
+    }
+    if (XMLAttribute)
+    {
+        strcat(buffArray, XMLAttribute);
+    }
+    strcat(buffArray, ">\n");
+    strcat(buffArray, "\t\t\t<input>");
+    if (!VALUE)
+    {
+        strcat(buffArray, "1");
+    }
+    else
+    {
+        strcat(buffArray, VALUE);
+    }
+    strcat(buffArray, "</input>\n");
+    strcat(buffArray, "\t\t\t<output>");
+    if (!OUTPUT)
+    {
+        strcat(buffArray, "CB1");
+    }
+    else
+    {
+        strcat(buffArray, OUTPUT);
+    }
+    strcat(buffArray, "</output>\n");
+    strcat(buffArray, "\t\t</button>\n");
+}
---
+++ src/toggle.cpp
@@ -0,0 +1,66 @@
+#include "phyphoxBleExperiment.h"
+#include "copyToMem.h"
+
+void PhyphoxBleExperiment::Toggle::setDefault(bool d)
+{
+    if (d)
+        copyToMem(&DEFAULT, "true");
+    else
+        copyToMem(&DEFAULT, "false");
+}
+
+void PhyphoxBleExperiment::Toggle::setOutputChannel(int b)
+{
+    ERROR = ERROR.MESSAGE == NULL ? err_checkUpper(b, 5, "setOutputChannel") : ERROR;
+    char tmp[20];
+    sprintf(tmp, "CB%i", b);
+    copyToMem(&OUTPUT, tmp);
+}
+
+void PhyphoxBleExperiment::Toggle::setChannel(int b)
+{
+    setOutputChannel(b);
+}
+
+void PhyphoxBleExperiment::Toggle::setXMLAttribute(const char *xml)
+{
+    ERROR = ERROR.MESSAGE == NULL ? err_checkLength(xml, 98, "setXMLAttribute") : ERROR;
+    copyToMem(&XMLAttribute, (" " + std::string(xml)).c_str());
+}
+
+void PhyphoxBleExperiment::Toggle::getBytes(char *buffArray)
+{
+    strcat(buffArray, "\t\t<toggle");
+    if (LABEL)
+    {
+        strcat(buffArray, " label=\"");
+        strcat(buffArray, LABEL);
+        strcat(buffArray, "\"");
+    }
+    else
+    {
+        strcat(buffArray, " label=\"toggle\"");
+    }
+    if (DEFAULT)
+    {
+        strcat(buffArray, " default=\"");
+        strcat(buffArray, DEFAULT);
+        strcat(buffArray, "\"");
+    }
+    if (XMLAttribute)
+    {
+        strcat(buffArray, XMLAttribute);
+    }
+    strcat(buffArray, ">\n");
+    strcat(buffArray, "\t\t\t<output>");
+    if (!OUTPUT)
+    {
+        strcat(buffArray, "CB1");
+    }
+    else
+    {
+        strcat(buffArray, OUTPUT);
+    }
+    strcat(buffArray, "</output>\n");
+    strcat(buffArray, "\t\t</toggle>\n");
+}
